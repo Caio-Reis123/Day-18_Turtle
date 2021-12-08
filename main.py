2cc9111af70c9c6ccc2c8a6ecc559886635eafa8
Caio-Reis123 <77801252+Caio-Reis123@users.noreply.github.com>
2021-12-08 17:45:32 -0300

    Concluído

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import Turtle, Screen, left, right
+import random
+
+tim = Turtle()
+tim.shape('turtle')
+tim.color('green')
+
+def change_color():
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    tim.color(R, G, B)
+
+tim.speed(11)
+tim.pensize(2)
+tim.hideturtle()
+
+# lados = 3
+
+# Exercício 1
+# while lados <= 10:
+#     for lado in range(lados):
+#         tim.forward(100)
+#         tim.right(360/lados)
+#     lados += 1
+
+#direction = random.choice(right, left)
+
+# Exercício 2
+# for _ in range (0, 40):
+#     direction = [0, 90, 180, 270]
+#     tim.forward(50)
+#     tim.setheading(random.choice(direction))
+#     change_color()
+
+x = -450
+y = -350
+tim.penup()
+tim.goto(x, y)
+
+for _ in range (0, 10):
+    for _ in range (0, 19):
+        tim.dot(10)
+        tim.forward(50)
+        change_color()
+    y += 50
+    tim.goto(x, y)
+
+screen = Screen()
+screen.exitonclick()
